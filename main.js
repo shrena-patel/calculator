3302d87f0b348f3c702a1adfc72062c56a16b00f
Shrena Patel <shrena.p@hotmail.co.uk>
2020-10-14 09:07:56 +1300

    operator functions working

--- main.js
+++ main.js
@@ -10,29 +10,53 @@ keys.addEventListener('click', event => {
     const key = event.target
     const keyValue = key.textContent
     const displayValue = display.textContent
-    const type = key.dataset.type
+    const { type } = key.dataset
+    const { previousKeyType } = calculator.dataset
 
     // Is this a number key?
     if (type == 'number') {
         if (displayValue == '0') {
             display.textContent = keyValue
         } else if (previousKeyType == 'operator') {
-
+            display.textContent = keyValue
         } else {
             display.textContent = displayValue + keyValue
         }
+
     }
 
 
     // Is this an operator key?
     if (type == 'operator') {
-        console.log(key)
-
-
-        calculator.dataset.previousKeyType = 'operator'
+        const operatorKeys = keys.querySelectorAll('[data-type="operator"]')
+        //change below to a map
+        operatorKeys.forEach(e => { e.dataset.state = '' })
+        key.dataset.state = 'selected'
+        
+        calculator.dataset.firstNumber = displayValue
+        calculator.dataset.operator = key.dataset.key
     }
 
+    if (type == 'equal') {
+        // perform a calculation
+        const firstNumber = Number(calculator.dataset.firstNumber)
+        const operator = calculator.dataset.operator
+        const secondNumber = Number(displayValue)
+        // console.log(firstNumber, operator, secondNumber)
 
 
+        let result = ''
+        if (operator == 'plus') result = firstNumber + secondNumber
+        if (operator == 'minus') result = firstNumber - secondNumber
+        if (operator == 'times') result = firstNumber * secondNumber
+        if (operator == 'divide') result = firstNumber / secondNumber
+        // console.log(result)
 
+        display.textContent = result
+
+
+    }
+
+    calculator.dataset.previousKeyType = type
 })
+
